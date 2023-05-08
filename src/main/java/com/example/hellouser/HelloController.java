40c7b4b164a36dfdd17445e1853071e8b8729187
JavierGarciaEstrada <baevaj@naver.com>
2023-05-07 19:36:50 -0600

    Creacion del tercer codigo con JavaFX

--- src/main/java/com/example/hellouser/HelloController.java
+++ src/main/java/com/example/hellouser/HelloController.java
@@ -2,13 +2,17 @@ package com.example.hellouser;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField nameField;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String name = nameField.getText();
+        welcomeText.setText("Hello " + name );
     }
 }
